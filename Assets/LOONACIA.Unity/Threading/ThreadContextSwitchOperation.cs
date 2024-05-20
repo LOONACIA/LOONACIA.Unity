8a49ce7df1a03893cb9d4556c20043929316e2f3
LOONACIA <loonacia@gmail.com>
2024-05-20 16:57:21 +0900

    Implement `ICriticalNotifyCompletion` in `ThreadContextSwitchOperation`

--- Assets/LOONACIA.Unity/Threading/ThreadContextSwitchOperation.cs
+++ Assets/LOONACIA.Unity/Threading/ThreadContextSwitchOperation.cs
@@ -1,9 +1,10 @@
 using System;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace LOONACIA.Unity.Threading
 {
-    public readonly struct ThreadContextSwitchOperation : IAwaitable<ThreadContextSwitchOperation>, IAwaiter
+    public readonly struct ThreadContextSwitchOperation : IAwaitable<ThreadContextSwitchOperation>, IAwaiter, ICriticalNotifyCompletion
     {
         private readonly bool _isSwitchingToMainThread;
         
@@ -24,10 +25,23 @@ namespace LOONACIA.Unity.Threading
             }
             else
             {
-                Task.Run(continuation);
+                ThreadPool.QueueUserWorkItem(obj => continuation(), null);
             }
         }
-        
+
+        public void UnsafeOnCompleted(Action continuation)
+        {
+            if (_isSwitchingToMainThread)
+            {
+                using var flowControl = ExecutionContext.SuppressFlow();
+                Dispatcher.Current.Enqueue(continuation);
+            }
+            else
+            {
+                ThreadPool.UnsafeQueueUserWorkItem(obj => continuation(), null);
+            }
+        }
+
         public void GetResult()
         {
         }
